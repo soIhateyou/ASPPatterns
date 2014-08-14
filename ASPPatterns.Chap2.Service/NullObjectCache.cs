8bb2c85b8dc88b60f1d4b50682f670dddbf6bbe7
soIhateyou <jszhaofeng@qq.com>
2014-08-14 09:08:22 +0800

    Null Object

---
+++ ASPPatterns.Chap2.Service/NullObjectCache.cs
@@ -0,0 +1,18 @@
+﻿namespace ASPPatterns.Chap2.Service
+{
+    public class NullObjectCache : ICacheStorage
+    {
+        public void Remove(string key)
+        {
+        }
+
+        public void Store(string key, object data)
+        {
+        }
+
+        public T Retrieve<T>(string key)
+        {
+            return default(T);
+        }
+    }
+}
